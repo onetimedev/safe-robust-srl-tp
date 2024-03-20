5ff97df89d91004c80268a3d970315cd396685de
Xavier Hickman <xavierhickman1234@gmail.com>
2024-03-20 14:32:46 +0000

    Removed redundant dependancies

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,4 @@
-conda create --name tf python=3.8
-conda activate tf
+#!/usr/bin/env bash
+conda create --name experiment-environment python=3.8
+conda activate experiment-environment
 pip install -r requirements.txt
